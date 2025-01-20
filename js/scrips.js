1239d4c64a1357bc765ddcbd4604f3dff025d95b
Fredrik <181095176+HuckerDuck@users.noreply.github.com>
2025-01-20 14:31:47 +0100

    En sista uppdatering inför imorgon. Nu ska Github Pages fixas

--- js/scrips.js
+++ js/scrips.js
@@ -1,6 +1,30 @@
+//1 -  Javascript för Hamburgare menyn i mobilen
+
 const hamburger = document.getElementById("hamburger");
 const navsmall = document.getElementById("navbarMobileList");
 
 hamburger.addEventListener("click", () => {
     navbarMobileList.classList.toggle("active");
+});
+
+//2 - Filtering av kategorier
+document.addEventListener("DOMContentLoaded", () => {
+    // Dropdown-menyn
+    const filterDropdown = document.getElementById("filter"); 
+    
+    // Alla artiklarna i listan
+    const articles = document.querySelectorAll(".article"); 
+
+    // 
+    filterDropdown.addEventListener("change", () => {
+        const selectedCategory = filterDropdown.value.toLowerCase(); 
+
+        // Loopar genom allt innehåll
+        articles.forEach((article) => {
+            article.style.display = 
+                selectedCategory === "all" || article.dataset.category.toLowerCase().includes(selectedCategory)
+                ? "block"
+                : "none";
+        });
+    });
 });
